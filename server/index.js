3026378d36dd9ca3673e7c5eeda26ed97bf4028c
Kathia Villavicencio <kathia.villavicencio1@outlook.com>
2024-06-16 21:46:52 -0500

    Change port

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 require("dotenv").config(); // Load environment variables from .env file
 const { db } = require("./db/models");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = require("./app");
 const seed = require("../script/seed"); // Ensure the correct path to seed.js
 
 const init = async () => {
   try {
+    console.log("Starting server initialization...");
     if (process.env.SEED === "true") {
+      console.log("Seeding the database...");
       await seed();
     }
+    console.log("Synchronizing the database...");
     await db.sync();
     // Start listening on the specified port
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   } catch (ex) {
-    console.error(ex);
+    console.error("Initialization failed:", ex);
     process.exit(1); // Exit the process with a failure code
   }
 };
