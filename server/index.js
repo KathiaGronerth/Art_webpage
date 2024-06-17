d18fd002037c2d9127715615e173c6b47040741d
Kathia Villavicencio <kathia.villavicencio1@outlook.com>
2024-06-16 21:30:46 -0500

    Updating port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config(); // Load environment variables from .env file
 const { db } = require("./db/models");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = require("./app");
 const seed = require("../script/seed"); // Ensure the correct path to seed.js
 
