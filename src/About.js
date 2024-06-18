d10f36a6cfdf7035e4b6be18f82bea3c6a218f4f
Kathia Villavicencio <kathia.villavicencio1@outlook.com>
2024-06-18 14:06:51 -0500

    Updating About.js

--- src/About.js
+++ src/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import about1 from "../public/about1.jpg";
+import about2 from "../public/about2.jpg";
 
 const About = () => {
   return (
@@ -6,8 +8,8 @@ const About = () => {
       <p>About the Artist</p>
       <div className="content">
         <div className="images">
-          <img src="about1.jpg" alt="Carl_Canga" />
-          <img src="about2.jpg" alt="Carl_Canga" />
+          <img src={about1} alt="Carl_Canga" />
+          <img src={about2} alt="Carl_Canga" />
         </div>
         <div className="description">
           <p style={{ fontWeight: "bold" }}>What can I tell you?</p>
