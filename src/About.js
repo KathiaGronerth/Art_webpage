91d8a4f39b6103f2eafe0171072c437e98d0d779
Kathia Villavicencio <kathia.villavicencio1@outlook.com>
2024-06-18 13:05:17 -0500

    Updating app.js for public/images

--- src/About.js
+++ src/About.js
@@ -6,8 +6,8 @@ const About = () => {
       <p>About the Artist</p>
       <div className="content">
         <div className="images">
-          <img src="about1.jpg" alt="Carl_Canga" />
-          <img src="about2.jpg" alt="Carl_Canga" />
+          <img src="/about1.jpg" alt="Carl_Canga" />
+          <img src="/about2.jpg" alt="Carl_Canga" />
         </div>
         <div className="description">
           <p style={{ fontWeight: "bold" }}>What can I tell you?</p>
